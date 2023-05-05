4ee4dabb4afe35de897fdab4218d13650d9db93e
rezaawp <rezaaawp@gmail.com>
2023-05-05 17:18:39 +0700

    update

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Choise;
 use App\Models\Division;
 use App\Models\Vote;
 use Illuminate\Http\Request;
@@ -25,20 +26,25 @@ class VoteController extends Controller
 
     function show()
     {
+        $pollId = 1;
         $division = Division::all();
+        $choises = Choise::where('poll_id', $pollId)->get();
         $namaDivisiTersedia = [];
         $resultPerDivisi = [];
-        $vote = Vote::with(['division', 'choise'])->where('poll_id', 1)->get()->groupBy('division.name');
+        $vote = Vote::with(['division', 'choise'])->where('poll_id', $pollId)->get()->groupBy('division.name');
         foreach ($division as $d) {
             array_push($namaDivisiTersedia, array('id' => $d['id'], 'name' => $d['name']));
-            if (isset($vote[$d['name']]))
+            if (isset($vote[$d['name']])) {
+
                 array_push($resultPerDivisi, [$d['name'] => collect($vote[$d['name']])->groupBy('choise.choise')]);
+            }
         }
 
         return $resultPerDivisi;
 
 
         // $voteBaru = collect([$vote])->map
+         
         return response()->json(['vote' => 'vote']);
     }
 }
